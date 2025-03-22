86f6a6134c6ae4ed07340bd83682b367af0068e8
Hieu Nguyen <hieunguyen@appvity.com>
2025-03-22 21:47:58 +0700

    FEATE: get list users

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -8,4 +8,25 @@ router.get('/checkstatus', (req, res, next) => {
     })
 })
 
+router.get('/users', (req, res, next) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'api ok',
+        metadata: [
+            {
+                id: 1,
+                name: 'Hieu Nguyen',
+            },
+            {
+                id: 2,
+                name: 'Messi'
+            },
+            {
+                id: 3,
+                name: 'Ronaldo'
+            }
+        ]
+    })
+})
+
 module.exports = router;
